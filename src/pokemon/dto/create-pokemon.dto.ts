be2021616df5e9f2034883711e58dc486f23390e
johan <johan>
2023-12-03 11:24:30 -0300

    feat: init project with first connection setup

--- src/pokemon/dto/create-pokemon.dto.ts
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -1,1 +1,12 @@
-export class CreatePokemonDto {}
+import { IsInt, IsPositive, IsString, Min, MinLength } from 'class-validator';
+
+export class CreatePokemonDto {
+  @IsInt()
+  @IsPositive({ message: 'Numero positivo' })
+  @Min(1, { message: 'Mayor que 0' })
+  readonly no: number;
+
+  @IsString()
+  @MinLength(3)
+  readonly name: string;
+}
--- src/pokemon/entities/pokemon.entity.ts
+++ src/pokemon/entities/pokemon.entity.ts
@@ -1,1 +1,14 @@
-export class Pokemon {}
+import { Document } from 'mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+@Schema()
+export class Pokemon extends Document {
+  @Prop({
+    unique: true,
+    index: true,
+  })
+  name: string;
+  number: number;
+}
+
+export const PokemonSchema = SchemaFactory.createForClass(Pokemon);
